5e74da33d90b9ee7e5373fb312b919f0d3f799f7
Unicare Health Developer <unicarevcs@users.noreply.github.com>
2017-11-27 08:45:42 +0800

    Update CalendarEvent.php

--- src/CalendarEvent.php
+++ src/CalendarEvent.php
@@ -1096,33 +1096,4 @@
 			return $result;
 		}
 	}
-
-	class CalendarEvent_TZSwitch
-	{
-		private $origtz, $newtz;
-
-		public function __construct($new)
-		{
-			$tz = (function_exists("date_default_timezone_get") ? @date_default_timezone_get() : @ini_get("date.timezone"));
-			if ($tz == "")  $tz = "UTC";
-
-			$this->origtz = $tz;
-			$this->newtz = $new;
-
-			if ($this->origtz != $this->newtz)
-			{
-				if (function_exists("date_default_timezone_set"))  @date_default_timezone_set($this->newtz);
-				else  @ini_set($this->newtz);
-			}
-		}
-
-		public function __destruct()
-		{
-			if ($this->origtz != $this->newtz)
-			{
-				if (function_exists("date_default_timezone_set"))  @date_default_timezone_set($this->origtz);
-				else  @ini_set($this->origtz);
-			}
-		}
-	}
 ?>
